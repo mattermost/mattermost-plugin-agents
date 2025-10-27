17590137e9a61c9f85d4fddf6b7a49956119ebcb
Christopher Speller <crspeller@gmail.com>
2025-10-27 17:15:32 +0100

    Lint

--- webapp/src/components/rhs/rhs.tsx
+++ webapp/src/components/rhs/rhs.tsx
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React, {useState, useEffect, useCallback} from 'react';
-import {FormattedMessage, useIntl} from 'react-intl';
+import {useIntl} from 'react-intl';
 import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components';
 
