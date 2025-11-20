4a891e23bdce53c783b7a20fbbc7ce2ab8d83f1b
Nick Misasi <nick.misasi@mattermost.com>
2025-11-20 11:43:31 -0500

    Turn off fullyParallel, but put back to 2 workers

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -13,14 +13,14 @@ import path from 'path';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 2 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
